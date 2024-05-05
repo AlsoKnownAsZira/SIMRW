58a1b54e27ce1d7e04ac92dbc0a01474665268ac
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-05-05 23:53:16 +0700

    feat: migrate dan seeding inventori

---
+++ database/seeders/InventoriSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InventoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $currenttimestamp = now();
+        $data = [
+            ['nama_barang' => 'Speaker', 'jumlah_barang' => '2', 'created_at' => $currenttimestamp],
+
+            ['nama_barang' => 'Microphone', 'jumlah_barang' => '7', 'created_at' => $currenttimestamp],
+            ['nama_barang' => 'Kabel olor', 'jumlah_barang' => '5', 'created_at' => $currenttimestamp],
+        ];
+        DB::table('t_inventori')->insert($data);
+    }
+}
