32a4a3e17c70c26504330cc44eb21c5b08268511
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-05-07 03:36:33 +0700

    feat: front end login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,36 +16,63 @@
             font-size: 14px;
             font-style: italic;
         }
+        .gradient-custom {
+    /* fallback for old browsers */
+    background: #833ab4;
+
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: -webkit-linear-gradient(to right, rgba(131, 58, 180, 1), rgba(253, 29, 29, 1), rgba(252, 176, 69, 1));
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(to right, rgba(131, 58, 180, 1), rgba(253, 29, 29, 1), rgba(252, 176, 69, 1));
+}
     </style>
 </head>
 <body>
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">Login</div>
-                <div class="card-body">
-                    <form id="login-form" method="post" action="{{ route('login.auth') }}">
-                        @csrf
+<section class="vh-200 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
 
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input id="username" type="text" class="form-control" name="username" required>
-                        </div>
+            <div class="mb-md-5 mt-md-4 pb-5">
 
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input id="password" type="password" class="form-control" name="password" required>
-                            <small id="passwordError" class="error-message hidden"></small>
-                        </div>
+              <h2 class="fw-bold mb-2 text-uppercase">Masuk</h2>
+              <p class="text-white-50 mb-5">Silahkan Masukkan nama pengguna dan kata sandi anda</p>
 
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
+              <form id="login-form" method="post" action="{{ route('login.auth') }}">
+                @csrf
+
+                <div data-mdb-input-init class="form-outline form-white mb-4">
+                    <label class="form-label" for="username">Nama Pengguna</label>
+
+                    <input type="text" id="username" name="username" class="form-control form-control-lg" required>
                 </div>
+
+                <div data-mdb-input-init class="form-outline form-white mb-4">
+                    <label class="form-label" for="password">Kata Sandi </label>
+                    <input type="password" id="password" name="password" class="form-control form-control-lg" required>
+                  <small id="passwordError" class="error-message hidden"></small>
+                </div>
+
+                <p class="small mb-5 pb-lg-2"><a class="text-blue-50" href="#!">Lupa password?</a></p>
+
+                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Masuk</button>
+              </form>
             </div>
+
+            <div>
+              <p class="mb-0">Belum Punya Akun? <a href="#!" class="text-blue-50 fw-bold">Daftar</a>
+              </p>
+            </div>
+
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+</section>
 
 <!-- Bootstrap JS -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
